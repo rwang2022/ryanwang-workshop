cb96f6d0768f090264ba1d8a78de825d0a9811c3
rwang2022 <90988076+rwang2022@users.noreply.github.com>
2021-10-07 22:22:29 -0400

    added 12_flask-jinja

---
+++ 12_flask-jinja/app.py
@@ -0,0 +1,21 @@
+# Wombats (Eliza Knapp, Noakai Aronesty, Ryan Wang)
+# SoftDev
+# K12 -- ...or The Only Constant is Change?
+# 2021-10-07
+
+from flask import Flask #Q0: What happens if you remove render_template from this line?
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "No hablo queso!"
+
+coll = [0,1,1,2,3,5,8]
+
+@app.route("/my_foist_template") #Q1: Can all of your teammates confidently predict the URL to use to load this page?
+def test_tmplt():
+    return render_template( 'model_tmplt.html', foo="fooooo", collection=coll, wombat="hello") #Q2: What is the significance of each argument?
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
