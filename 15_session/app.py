6697b7fc8609555c3d35b2c7e13e8518828ebcce
Ryan Wang <rwang22@stuy.edu>
2021-10-19 20:40:07 -0400

    added 15_session

--- 15_session/app.py
+++ 15_session/app.py
@@ -7,64 +7,59 @@
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
+from flask import session
 
 #the conventional way:
 #from flask import Flask, render_template, request
 
 app = Flask(__name__)    #create Flask object
-
-
-'''
-trioTASK:
-~~~~~~~~~~~ BEFORE RUNNING THIS, ~~~~~~~~~~~~~~~~~~
-...read for understanding all of the code below.
-Some will work as written; other sections will not. Can you predict which?
-Devise some simple tests you can run to "take apart this engine," as it were.
-Execute your tests. Process results.
-PROTIP: Insert your own in-line comments wherever they will help your future self and/or current teammates understand what is going on.
-'''
+app.secret_key = "dogs say woof"
 
 @app.route("/") #, methods=['GET', 'POST'])
 def disp_loginpage():
-    '''
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app) #<Flask 'app'>
-    print("***DIAG: request obj ***")
-    print(request)
-    print("***DIAG: request.args ***")
-    print(request.args)
-    #empty dict cuz user hasn't submitted anything yet
-    #request.args['username'] doesn't exist yet, cuz user didn't submit username yet
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
-    print("***DIAG: request.headers ***")
-    print(request.headers)
-    '''
-    '''
-        form prompts user to enter username in webpage;
-        <name="username"> saves the user's inputted name under the key "username" in the ImmutableMultiDict
-        <form action="/auth"> after user submits, takes the user to <host>/auth?username=...
-    '''
-    return render_template( 'login.html' )
+    # check whether or not it is already is session
+    if not session.get(username):
+        # if not, just bring to the login page
+        return render_template("login.html")
+    else:
+        # if it is, bring to the logged-in page (response.html)
+        return render_template("response.html")
 
 
 @app.route("/auth") # , methods=['GET', 'POST'])
 def authenticate():
     #hard coding single username and password
-    actualUsername = "threeCoffeePeanuts"
-    actualpassword = "oneWalnutLatte"
-    username = request.args['username']
-    password = request.args['password']
-    '''
-        use response.html as template
-        method (called in response.html) is actually the request.method (which is get)
-        username (called in response.html) is actually username (from this python file)
-    '''
-    if (username == actualUsername) and (password == actualpassword): #comparing input and hard coded user/pass
-        return render_template('response.html', method=request.method, username=username, password = password)  #response to correct user/pass
+    real_user = "coffee"
+    real_passwd = "peanut"
+
+    print(session)
+    
+    # using conditional in order to make GET/POST fail-safe
+    if request.method == "GET":
+        username = request.args['username']
+        password = request.args['password']
+    elif request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+    
+    session[username] = password
+    print(session)
+
+    # if username and password are a correct pair, we send them to response.html (logs you in)
+    if (username == real_user and password == real_passwd):
+        return render_template('response.html', method=request.method, username=username)
+    
+    # login failed because of wrong pass, tells you error at the bottom (at login.html)
+    elif (username == real_user and password != real_passwd):
+        error = "user exists but wrong password"
+        return render_template( 'login.html', error=error)
+
+    # login fails because username isn't correct / doesn't exist
     else:
-        return render_template( 'login.html' ) #response to wrong user/pass
+        error = "Error: that username does not exist"
+        return render_template( 'login.html', error=error)
+    
+
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
